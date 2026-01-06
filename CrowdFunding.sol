8fd1fdd7bc142cbd946920ce35a42b23503754e4
giang19062001 <meobapgiang19062001@gmail.com>
2026-01-06 11:13:48 +0700

    Push

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -9,9 +9,9 @@ import {PriceConverter} from "./PriceConverter.sol";
 // msg.value là giá trị Wei, ETH,.. khi gọi contract này --> chỉ hoạt động trong hàm đánh dấu là 'payable'
 
 contract Crowdfunding {
-    error NotOwner();
+    error NotOwner(); // khai báo lỗi để ném cho revert -> khuyên dùng -> tiết kiệm gas thay vì ném lỗi chuỗi 
 
-    using PriceConverter for uint256;
+    using PriceConverter for uint256; // khai báo sử dụng thư viện, chỉ Áp dụng library cho các kiểu dữ liệu uint256 khớp với tham số type của hàm trong library
 
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
@@ -20,7 +20,10 @@ contract Crowdfunding {
     address public immutable i_owner;
 
     // constant -> hằng số không thể thay đổi
-    uint256 public constant MINIMUM_USD = 1 * 10 ** 18; //  Solidity, các token thường sử dụng 18 số thập phân (ví dụ: 1 ETH = 1e18 wei)
+    uint256 public constant MINIMUM_USD = 1e18; // 1 USD với đơn vị Wei (1000000000000000000)
+
+
+    // Event là một cách để contract gửi thông báo đến bên ngoài blockchain (ví dụ: frontend, backend, hoặc các contract khác).
     event Funded(address indexed funder, uint256 amount);
     event Withdrawn(address indexed owner, uint256 amount);
 
@@ -34,14 +37,22 @@ contract Crowdfunding {
     //     i_owner = own
     // }
 
+
+    // 'modifier' -> từ khóa này giúp tái sử dụng logic ở nhiều hàm khác nhau ( hàm withdraw() bên dưới tái sử dụng nội dung code của  onlyOwner() này)
     modifier onlyOwner() {
         // chỉ chủ sở hữu mới có thể rút tiền từ contract về ví của mình
+        // 'revert' -> hoàn tác tác vụ và quăng lỗi
+        // dung  revert error() thay vì revert('bạn ko phải chủ sở hữu') -> tiết kiệm gas -> khuyên dùng
         if (msg.sender != i_owner) revert NotOwner();
+        // !!!  _ đặt kí hiệu này phía sau nội dung trong hàm được modifier 
+        // -> đồng nghĩa là các hàm nào tái sử dụng hàm onlyOwner() sẽ chạy sau khi đoạn code trong onlyOwner() chạy xong
+        // !!! _ đặt kí hiệu này phía trước nội dung trong hàm được modifier -> sẽ chạy đoạn code trong onlyOwner() này sau đoạn code gọi nó
         _;
     }
 
-    // !!! Để 1 contract có thể nhận ETH, Wei,.. bắt buộc phải có 2 hàm này: fallback(), receive()
-    // !!! Nếu contract hiện tại chỉ là contract gửi ETH, Wei - Ko có chức năng nhận -> ko cần khai báo 2 hàm này
+    // !!! Để 1 contract có thể NHẬN ETH, Wei,.. mà ko contract ko có tự viết hàm  'payable'  NHẬN -> bắt buộc phải có 2 hàm này: fallback(), receive()
+    // !!! Nếu contract hiện tại Ko có chức năng NHẬN ETH, Wei,... -> ko cần khai báo 2 hàm này, cũng như không cần tự viết viết hàm 'payable' 
+
     // Gửi ETH,.. -> nếu msg.data (calldata) có -> fallback()
     fallback() external payable {
         fund();
@@ -51,29 +62,41 @@ contract Crowdfunding {
         fund();
     }
 
-    // payable chỉ thêm cho các hàm nào có GỬI/NHẬN giá trị Wei, ETH,...
+    // !!! fallback() và receive() chỉ xử lý các transaction gửi ETH trực tiếp đến địa chỉ contract mà không cần thông qua gọi hàm 'payable' tự viết nào.
+    // !!! fund() xử lý các transaction gửi ETH thông qua hàm 'payable' tự viết
+
+    // payable chỉ cho biết rằng  hàm này có thể NHẬN giá trị Wei, ETH,...
     function fund() public payable {
         // 'require' kiểm tra điều kiện và ném lỗi sẽ thấy ở phần 'status' -> có thể check trên 'etherscan'
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
+
+        // !!!! msg.value.getConversionRate() thay vì getConversionRate(msg.value) vì ta đang dùng library
+        // ---> nó chỉ cho phép ~ giá trị có type uint256 gọi hàm của library đó
+        // ---> nên phải viết như gọi 1 method như này, solidity tự khắc biết msg.value sẽ là tham số đầu tiên cho hàm getConversionRate() đó
+        // @@@@@ require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");  // so sánh giá thực tế lấy từ chainlink so sánh với giá điều kiện 
+        addressToAmountFunded[msg.sender] += msg.value; // cộng đồn số tiền đã donate cho người donate hiện tại
         funders.push(msg.sender);
-        emit Funded(msg.sender, msg.value);
+        emit Funded(msg.sender, msg.value); // gửi event ra ngoài on-chain ( kiểm tra phần logs )
     }
 
+    // onlyOwner -> gọi đoạn code tái sử dụng
     function withdraw() public onlyOwner {
         // address(this).balance  ---> giá trị balancer mà địa chỉ contract hiện tại đang nắm giữ
+        //  address(this) --->  trả ra địa chỉ của contract này
         uint256 balance = address(this).balance;
+
+        // reset mapping của funder với số tiền họ từng donate về 0
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
 
-        funders = new address[](0);
+        funders = new address[](0); // reset mảng funders về rỗng sau khi rút hết tiền 
 
-        // payable(msg.sender) để 1 địa chỉ có thể gọi các hàm chuyển tiền có sẵn 
+        // payable(msg.sender) để 1 địa chỉ có thể gọi các hàm chuyển tiền có sẵn cho chính địa chỉ đó
+        // Các hàm có sẵn của payable(address) như: transfer, send, call
         (bool callSuccess,) = payable(msg.sender).call{value: balance}("");
         require(callSuccess, "Call failed");
 
-        emit Withdrawn(msg.sender, balance);
+        emit Withdrawn(msg.sender, balance); // gửi event ra ngoài on-chain ( kiểm tra phần logs )
     }
 }
--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,20 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+//Chainlink chỉ hoạt động trên testnet hoặc mainnet, ko hoạt động local remix
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+
+// dùng 'library' giúp tái sử dụng code ở nhiều chỗ -> tiết kiễm gas
 library PriceConverter {
     function getPrice() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 answer,,,) = priceFeed.latestRoundData();
+        // địa chỉ '0x694AA1769357215DE4FAC081bf1f309aDC325306' là là của cặp 'ETH / USD' trong price-feeds của chainlink oracle
+        // địa chỉ này cho biết giá thực tế của 1 ETH là bao nhiêu USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (
+             /* uint80 roundId */,
+            int256 answer,
+            ,
+            /*uint256 startedAt*/
+            ,
+            /*uint256 updatedAt*/
+            /*uint80 answeredInRound*/
+        ) =  priceFeed.latestRoundData(); // hàm trả về giá trị thực tể của USD của 1 đồng coin nào đó dựa vào địa chỉ  'feed price'
         require(answer > 0, "Invalid price data");
-        // nhận về 8 digits, thêm vào 10 số 0 để lấy thành 18 digits
-        return uint256(answer) * 1e10;
+        // Vì chainlink luôn trả về 8 decimals
+        // nhưng Solidity xài 18 decimals
+        // decimal là những số đứng sau 4 số đầu ( số thực ) VD: 200000000000 là 2000.00 -> 00000000 (8 decimals)
+        // thêm vào 10 số 0 để lấy thành 18 decimals VD: 200000000000 (8 decimals) là 2000.00 ->  2000000000000000000000  (18 decimals) là 2000.00
+        return uint256(answer) * 1e10; // đơn vị theo Wei
     }
 
-    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        // chuẩn vẫn là 18 decimals, vì có 2 số 18 decimals nhân với nhau thì khả năng thành 36 decimals
+        // nên chia cho 1e18 để lấy lại 18 decimals
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // đơn vị theo Wei
         return ethAmountInUsd;
     }
 }
